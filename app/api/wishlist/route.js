1318155088c8354b6909dd17ac1ef2631768b454
apnashoes <apnashoes549@gmail.com>
2026-05-13 11:53:46 +0500

    Fixed next-auth import

--- app/api/wishlist/route.js
+++ app/api/wishlist/route.js
@@ -1,6 +1,6 @@
 import { connectDB } from "@/lib/db";
 import User from "@/models/User";
-import { getServerSession } from "next-auth/next";
+import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
 
 // GET Wishlist
